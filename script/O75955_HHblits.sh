c21c14aaf196660f3bd793b0f2322f4f0ea2d4b5
MinION <minion@rcepc10.ens.fr>
2021-01-28 13:02:18 +0100

    script corrigé

--- script/O75955_HHblits.sh
+++ script/O75955_HHblits.sh
@@ -1,11 +1,16 @@
 #run alignement on the database to found templats
-hhblits -cpu 4 -i O75955.fasta. -d databases/uniclust30 -oa3m  O75955.a3m
+hhblits -cpu 30 -i O75955.fasta -d ~/Desktop/perso_lynda/databases/uniclust30/UniRef30_2020_06_hhsuite/UniRef30_2020_06 -oa3m  O75955.a3m
+
+
+
+
+
 
 #to add secondary structure information to the MSA
-addss.pl  O75955.a3m
+#addss.pl  O75955.a3m
 
 #generate a hidden Markov model (HMM) from this MSA
-hhmake -i  O75955.a3m
+#hhmake -i  O75955.a3m
 
 #to convert on fasta file
-reformat.pl a3m fas  O75955.a3m  O75955_blits.fas
+#reformat.pl a3m fas  O75955.a3m  O75955_blits.fas
